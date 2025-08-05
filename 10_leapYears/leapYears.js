9ab522bca286c658394d909cfdf8958097893d95
Luís Carvalho <153909596+LuisCarvalhoGit@users.noreply.github.com>
2025-08-05 13:26:37 +0000

    09-10-11

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,4 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    // leap year -> year % 4 === 0
+    // if year % 100 === 0, not leap year
+    // if year % 100 === 0 && year % 400 === 0, leap year
+
+    if(year % 4 === 0 && (!(year % 100 === 0) || year % 400 === 0)) {
+        return true
+    } else {
+        return false
+    }
+        
+
 
 };
 
--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+
+  let celsius = Math.round(((fahrenheit - 32) / 1.8) * 10) / 10
+  return celsius
 };
 
-const convertToFahrenheit = function() {
+
+
+const convertToFahrenheit = function(celsius) {
+
+  let fahrenheit = Math.round(((celsius * 1.8) + 32) * 10) / 10
+  return fahrenheit
 };
 
 // Do not edit below this line
