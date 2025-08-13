1ebb88f0f5f0fcd0d089a1697611d57fab130145
Luis Carvalho <pro.luis.carvalho@gmail.com>
2025-08-13 18:27:45 +0100

    12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function(n1, n2) {
+	return n1 + n2
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, n2) {
+	return n1 - n2
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, current) => sum += current, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((multi, current) => multi *= current, 1)
 };
 
-const power = function() {
-	
+const power = function(n1, n2) {
+	return n1 ** n2
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num === 0) return 1
+	return num * factorial(num - 1)
 };
 
 // Do not edit below this line
