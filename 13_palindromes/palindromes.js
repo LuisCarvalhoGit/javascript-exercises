85c719912c3c2e71f6377aa6e3bb0492d2748170
Luis Carvalho <pro.luis.carvalho@gmail.com>
2025-08-13 22:19:26 +0100

    13-16

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    const validChars = 'abcdefghijklmnopqrstuvwxyz1234567890'
+
+    const cleanString = string
+        .toLowerCase()
+        .split('')
+        .filter(value => validChars.includes(value))
+        .join('')
+
+    const reverseString = cleanString.split('').reverse().join('')
+
+    if(cleanString === reverseString)
+        return true
+    else
+        return false
 };
 
 // Do not edit below this line
--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
 
+    n = +n
+    if (n === 0) return 0;
+    if (n === 1) return 1;
+    if (n < 0) return "OOPS"
+
+    return fibonacci(n-1) + fibonacci(n-2)
 };
 
 // Do not edit below this line
