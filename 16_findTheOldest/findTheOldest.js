cc36e23f96b846785e9320801cbea6ff55e2dc49
Luis Carvalho <pro.luis.carvalho@gmail.com>
2025-08-13 22:18:59 +0100

    13-16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,4 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+    let oldest = people[0]
+    let year = new Date().getFullYear()
+
+    for (let person of people) {
+
+        const personDeathYear = person.yearOfDeath ?? year;
+        const oldestDeathYear = oldest.yearOfDeath ?? year;
+
+        const personAge = personDeathYear - person.yearOfBirth;
+        const oldestAge = oldestDeathYear - oldest.yearOfBirth;
+
+        if (personAge > oldestAge)
+            oldest = person
+
+        
+    }
+
+    return oldest
 
 };
 
